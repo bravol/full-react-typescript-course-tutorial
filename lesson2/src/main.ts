26e579b8d8c241e7941f9c8d5d3e9d1b39c29345
Lumala Brian <lumalabravo@gmail.com>
2024-04-05 19:23:44 +0300

    finished all the data types in lesson 2

--- lesson2/src/main.ts
+++ lesson2/src/main.ts
@@ -4,6 +4,17 @@ let isLoaidng: boolean;
 
 let album: any;
 
+let albums: string | number; //this is a union type
+
 isLoaidng = true;
 
 album = 2;
+//adding numbers
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /^[a-zA-Z0-9]+$/;
