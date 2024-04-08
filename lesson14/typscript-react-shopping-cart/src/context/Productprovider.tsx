812c7bf018df8f45bb77e2175c153c0eb00c4705
Lumala Brian <lumalabravo@gmail.com>
2024-04-08 20:21:58 +0300

    Create Productprovider.tsx

---
+++ lesson14/typscript-react-shopping-cart/src/context/Productprovider.tsx
@@ -0,0 +1,29 @@
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+
+const initState: ProductType[] = [
+  {
+    sku: "item0001",
+    name: "Widget",
+    price: 9.99,
+  },
+  {
+    sku: "item0002",
+    name: "Premium Widget",
+    price: 19.99,
+  },
+  {
+    sku: "item0003",
+    name: "Deluxe Widget",
+    price: 29.99,
+  },
+];
+
+export type UseProductContextType = { products: ProductType[] };
+
+const initContextState: UseProductContextType = {
+  products: [],
+};
