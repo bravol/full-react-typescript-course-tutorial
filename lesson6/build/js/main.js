1d965a301a6a12df4991f378c30ae33c6cdb5f1e
Lumala Brian <lumalabravo@gmail.com>
2024-04-08 11:03:18 +0300

    finished lesson6 about classes

--- lesson6/build/js/main.js
+++ lesson6/build/js/main.js
@@ -13,7 +13,7 @@
 //   }
 // }
 class Coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = "JS") {
         this.name = name;
         this.music = music;
         this.age = age;
@@ -23,4 +23,79 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello, I'm ${this.age} years old`;
+    }
 }
+const Brian = new Coder("Brian", "Rock", 25, "JS");
+const Lumala = new Coder("Brian", "Rock", 25);
+console.log(Brian.getAge());
+console.log(Brian.name);
+// console.log(Brian.lang);
+// console.log(Brian.age);
+class webDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new webDev("mac", "Sara", "Pop", 25);
+console.log(Sara.getLang());
+console.log(Sara.getAge());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play("strums"));
+/////////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps("John");
+const Jimmy = new Peeps("Jimmy");
+const Bravol = new Peeps("Bravol");
+console.log(Peeps.count);
+console.log(John.id);
+///getters and setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else {
+            throw new Error("Data must be an array of strings");
+        }
+    }
+}
+const Mybands = new Bands();
+Mybands.data = ["Rammstein", "Nirvana"];
+console.log(Mybands.data);
+Mybands.data = [...Mybands.data, "Led Zeppelin"];
+console.log(Mybands.data);
+////////////////////////////////////////////////////////////////////
--- lesson6/src/main.ts
+++ lesson6/src/main.ts
@@ -14,15 +14,129 @@
 // }
 
 class Coder {
+  secondLang!: string;
   constructor(
     public name: string,
     public music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = "JS"
   ) {
     this.name = name;
     this.music = music;
     this.age = age;
     this.lang = lang;
   }
+
+  public getAge() {
+    return `Hello, I'm ${this.age} years old`;
+  }
 }
+
+const Brian = new Coder("Brian", "Rock", 25, "JS");
+const Lumala = new Coder("Brian", "Rock", 25);
+console.log(Brian.getAge());
+console.log(Brian.name);
+// console.log(Brian.lang);
+// console.log(Brian.age);
+
+class webDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
+
+const Sara = new webDev("mac", "Sara", "Pop", 25);
+console.log(Sara.getLang());
+console.log(Sara.getAge());
+// console.log(Sara.lang);
+// console.log(Sara.age);
+
+//////////////////////////////////////////////////
+//IMPELEMENTING INTERFACE TO A CLASS
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+  play(action: string): string {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const Page = new Guitarist("Jimmy", "guitar");
+
+console.log(Page.play("strums"));
+
+/////////////////////////////////////////////////////
+
+class Peeps {
+  static count: number = 0;
+
+  static getCount() {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const Jimmy = new Peeps("Jimmy");
+const Bravol = new Peeps("Bravol");
+
+console.log(Peeps.count);
+console.log(John.id);
+
+///getters and setters
+
+class Bands {
+  private dataState: string[] = [];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else {
+      throw new Error("Data must be an array of strings");
+    }
+  }
+}
+
+const Mybands = new Bands();
+Mybands.data = ["Rammstein", "Nirvana"];
+console.log(Mybands.data);
+Mybands.data = [...Mybands.data, "Led Zeppelin"];
+console.log(Mybands.data);
+
+////////////////////////////////////////////////////////////////////
