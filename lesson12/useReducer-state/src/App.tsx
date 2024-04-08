8cd837afe43f23886c9050a8e1a543dee1684dee
Lumala Brian <lumalabravo@gmail.com>
2024-04-08 19:31:11 +0300

    lesson 12 finished

--- lesson12/useReducer-state/src/App.tsx
+++ lesson12/useReducer-state/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hello</div>;
+  return (
+    <>
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+    </>
+  );
 }
 
 export default App;
