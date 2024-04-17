8a128ce37c446da31adf01fb3ad4a662e133bff1
Lumala Brian <lumalabravo@gmail.com>
2024-04-17 16:52:36 +0300

    functions in type annotations and inference finished

---
+++ new/basics/syntaxPlusFeatues/annotations/functions.ts
@@ -0,0 +1,75 @@
+//arrow functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+//anonymous function
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+//void and never means there will be no return value
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+//returning a string
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const throwError3 = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+//destructuring with annotation
+const forecast = {
+  date: new Date(),
+  weather: "sunny",
+};
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+logWeather(forecast);
+
+const profile = {
+  name: "alex",
+  age: 20,
+
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age, name }: { age: number; name: string } = profile;
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
