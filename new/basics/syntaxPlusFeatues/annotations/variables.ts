4eda03d861ca8cff24baaf662179319e4e0868fc
Lumala Brian <lumalabravo@gmail.com>
2024-04-17 16:18:00 +0300

    when to use annotations

--- new/basics/syntaxPlusFeatues/annotations/variables.ts
+++ new/basics/syntaxPlusFeatues/annotations/variables.ts
@@ -1,5 +1,7 @@
 //we tell typescript what type a value is
 let apples: number = 5;
+//type inference let apples = 5;   //typescript figures out the type of a value
+
 apples = 10;
 
 let speed: string = "fast";
@@ -25,3 +27,34 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// when to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
