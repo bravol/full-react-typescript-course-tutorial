0022a6561e4163a5c9f08bdae9c100d9f1275d92
Lumala Brian <lumalabravo@gmail.com>
2024-04-17 15:41:51 +0300

    type annotations

---
+++ new/basics/syntaxPlusFeatues/annotations/variables.ts
@@ -0,0 +1,27 @@
+//we tell typescript what type a value is
+let apples: number = 5;
+apples = 10;
+
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
---
+++ new/basics/syntaxPlusFeatues/features/types.ts
@@ -0,0 +1,14 @@
+const today = new Date();
+
+today.getMonth();
+
+const person = {
+  name: "Max",
+  age: 30,
+};
+
+person.name.toUpperCase();
+
+class Color {}
+
+const red = new Color();
