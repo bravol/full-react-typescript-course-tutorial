7465aff0c9f4f626b9dec103a0ea4238f1688125
Lumala Brian <lumalabravo@gmail.com>
2024-04-18 16:45:14 +0300

    sorting arigolithm project

---
+++ new/projects/sort/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hey there!");
+const logSomething = () => {
+    console.log("something");
+};
---
+++ new/projects/sort/src/index.ts
@@ -0,0 +1,5 @@
+console.log("hey there!");
+
+const logSomething = () => {
+  console.log("something");
+};
