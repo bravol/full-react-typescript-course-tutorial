19b3b0a58b4c629769914703c3d917dde5403793
Lumala Brian <lumalabravo@gmail.com>
2024-04-25 11:05:41 +0300

    building web interface using pure typescript

--- new/projects/web/dist/src.f10117fe.js
+++ new/projects/web/dist/src.f10117fe.js
@@ -117,9 +117,43 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log("Hi there");
-},{}],"../../../../../../.nvm/versions/node/v16.20.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+var User = /** @class */function () {
+  function User(data) {
+    this.data = data;
+  }
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+  return User;
+}();
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var User_1 = require("./models/User");
+var user = new User_1.User({
+  name: "brian",
+  age: 30
+});
+user.set({
+  name: "bravol"
+});
+console.log(user.get("name"));
+console.log(user.get("age"));
+},{"./models/User":"src/models/User.ts"}],"../../../../../../.nvm/versions/node/v16.20.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- new/projects/web/src/index.ts
+++ new/projects/web/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const user = new User({ name: "brian", age: 30 });
+user.set({ name: "bravol" });
+console.log(user.get("name"));
+console.log(user.get("age"));
